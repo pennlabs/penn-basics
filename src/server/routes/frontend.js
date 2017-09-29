9a934ac26c0faaaeb9e7369d17a18f95d8397b83
arun <arun.kirubarajan@gmail.com>
2017-09-29 13:36:31 -0400

    Add missing semi-colons

--- src/server/routes/frontend.js
+++ src/server/routes/frontend.js
@@ -5,7 +5,7 @@ export default function frontendRouter(DB){
     //frontend routes
     router.get('/', (req,res) => {
         res.sendFile(path.join(__basedir, 'public', 'site.html'));
-    })
+    });
 
     return router;
 }
