9087aea9933704e5b972b4796918c6387a807d12
ccabo1 <ccabo@seas.upenn.edu>
2017-10-14 14:52:47 -0400

    starting with hamburger menu

---
+++ src/shared/components/shared/nav/Menu.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Menu extends Component {
+    render(){
+        return(
+            <div id="sidebar-toggle">
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </div>
+        )
+    }
+}
+
+export default Menu;
--- src/shared/components/shared/nav/Nav.js
+++ src/shared/components/shared/nav/Nav.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import Links from './Links';
+import Menu from './Menu';
 
 class Nav extends Component {
     render(){
         return(
             <nav className="navbar">
+              <Menu />
               <h1>
                 PennCentral
               </h1>
