e048446b59f806f0713d530531800564b6a4f590
ccabo1 <ccabo@seas.upenn.edu>
2017-10-19 19:26:18 -0400

    Toggle filter options

--- src/shared/components/shared/filter/Filter.js
+++ src/shared/components/shared/filter/Filter.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 
 class Filter extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    var filterContent = document.getElementById("filterContent");
+    filterContent.classList.toggle("active");
+  }
+
   render(){
     return(
       <div className="filter">
-        <div className="filterToggle hover">
-          Filter
+        <div className="filterToggleWrapper">
+          <div className="filterToggle hover" onClick={this.handleClick}>
+            <i className="fa fa-filter"></i> Filter
+          </div>
         </div>
 
-        <div className="columns is-desktop display-none">
+        <div className="columns is-desktop" id="filterContent">
           <div className="column">
             <h2>Dank</h2>
             <ul>
