62fef81ba51b3ee62bf0288f8470cd3c4ad0cd48
ccabo1 <ccabo@seas.upenn.edu>
2017-10-19 20:06:36 -0400

    Adding state to checkboxes

--- src/shared/components/shared/filter/Check.js
+++ src/shared/components/shared/filter/Check.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 
 class Check extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {active: false};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState({active: !this.state.active});
+  }
+
   render(){
     return(
-      <div className="checkWrapper">
-        <span className="check"></span> <p>{this.props.description}</p>
+      <div className="checkWrapper" onClick={this.handleClick}>
+        <span className={ this.state.active ? "check active" : "check"}></span> <p>{this.props.description}</p>
       </div>
     )
   }
