3b1bc139cb0066cf6543d209126643ac25d52f6d
ccabo1 <ccabo@seas.upenn.edu>
2017-10-21 15:54:44 -0400

    fixed formatting bug

--- src/shared/components/shared/card/Card.js
+++ src/shared/components/shared/card/Card.js
@@ -3,32 +3,36 @@ import React, {Component} from 'react';
 class Card extends Component {
   render(){
     const content = (
-      <div className="column is-half-desktop">
-        <div className="card">
-          <div className="card-content">
-            <p className="title">
-              { this.props.title }
+      <div className="card">
+        <div className="card-content">
+          <p className="title">
+            { this.props.title }
+          </p>
+          { this.props.subtitle ?
+            <p className="subtitle medium-gray-text">
+              { this.props.subtitle}
             </p>
-            { this.props.subtitle ?
-              <p className="subtitle medium-gray-text">
-                { this.props.subtitle}
-              </p>
-              :
-              ''
-            }
-          </div>
+            :
+            ''
+          }
         </div>
       </div>
     );
 
     if (this.props.url) {
       return (
-        <a href={this.props.url}>
-          {content}
-        </a>
+        <div className="column is-half-desktop">
+          <a href={this.props.url}>
+            {content}
+          </a>
+        </div>
       );
     } else {
-      return content;
+      return (
+        <div className="column is-half-desktop">
+          {content}
+        </div>
+      );
     }
   }
 }
