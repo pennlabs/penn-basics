cae7bde38b93be604c383495e9192a5c441d6d85
ccabo1 <ccabo@seas.upenn.edu>
2017-10-21 15:14:40 -0400

    404 styling

--- src/shared/components/App.js
+++ src/shared/components/App.js
@@ -6,7 +6,13 @@ import Footer from './shared/footer/Footer';
 import Card from './shared/card/Card';
 
 const NotFound = () => (
-  <h1> Content not found </h1>
+  <div>
+    <h1 className="is-size-3 medium-gray-text">404: Content not found</h1>
+    <p>
+      It seems like the content you are looking for was either moved or does not exist.
+    </p>
+    <a href="/" className="btn marg-top-1">Back to home</a>
+  </div>
 );
 
 const Home = () => (
