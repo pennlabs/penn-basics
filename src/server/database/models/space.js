3c3e0b00a3fb7af04826728301c3a06c88285964
arun <arun.kirubarajan@gmail.com>
2017-11-10 07:18:05 -0500

    Remove space model definition

--- src/server/database/models/space.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SpaceSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  location: String
-});
-
-module.exports = mongoose.model('Space', SpaceSchema);
