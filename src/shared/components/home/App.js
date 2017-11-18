6dd3dfd143373093115ac59cf31726efc8959ca9
Tiffany <tifchang@wharton.upenn.edu>
2017-11-18 15:42:59 -0500

    fixed homepage loading

--- src/shared/components/home/App.js
+++ src/shared/components/home/App.js
@@ -1,19 +1,11 @@
 import React, {Component} from 'react';
 import Card from '../shared/card/Card';
+import Home from './Home';
 
 class App extends Component {
   render(){
     return(
-      <div>
-        <div className="columns is-desktop">
-          <Card title="Dining" subtitle="Get some grubbbb" url="/dining" />
-          <Card title="Laundry" subtitle="Clean yo self upp" url="/laundry" />
-        </div>
-        <div className="columns is-desktop">
-          <Card title="Study spaces" subtitle="Get studious af" url="/studyspaces" />
-          <Card title="Reserve a room" subtitle="Assemble the boys" url="/reservations" />
-        </div>
-      </div>
+      <Home />
     )
   }
 }
