13bcc4dbe981692bb3957726ec4a3f85a89486b4
arun <arun.kirubarajan@gmail.com>
2017-11-18 15:57:34 -0500

    Add mongo abstraction layer for spaces routes

--- src/server/database/db.js
+++ src/server/database/db.js
@@ -1,1 +1,36 @@
+const moment = require('moment');
 import mongoose from './mongoose-connect';
+import Space from './models/space';
+
+// params: none
+function findAllSpaces() {
+  return Space.find();
+}
+
+// params: filter object = (open: Boolean, outlets: Integer, noise: Integer, groups: Integer)
+function filterSpaces(open, outletLevel, quietLevel, groupLevel) {
+  if (open) {
+    const day = moment().day(), hour = moment().hour();
+    return Space.find({start: {$lte: hour}, end: {$gt: hour}, outlets: {$gte: outletLevel}, 
+      quiet: {$gte: quietLevel}, groups: {$gte: groupLevel}});
+  } else {
+    return Space.find({outlets: {$gte: outletLevel}, quiet: {$gte: quietLevel}, groups: {$gte: groupLevel}});
+  }
+}
+
+// params: spaceId
+function getSpace(space) {
+  return Space.findOne(space);
+}
+
+// params: new space
+function insertSpace(space) {
+  return new Space(space).save();
+}
+
+export default {
+  filterSpaces,
+  getSpace,
+  insertSpace,
+  findAllSpaces
+}
