b113055f4b41a505b999fa2d08ea2f4bd18e0fb2
Aaron Michaels <aaronmichaels@gmail.com>
2018-02-16 19:39:44 -0500

    Sidebar update
    add ability to collapse sidebar

--- src/shared/components/shared/sidebar/Sidebar.js
+++ src/shared/components/shared/sidebar/Sidebar.js
@@ -1,50 +1,72 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import uuid from 'uuid/v4';
 import SidebarSection from './SidebarSection';
 
 class Sidebar extends Component {
+
+  static defaultProps = {
+    sections: [{
+      "title": "Open now",
+      "links": [
+        {
+          "name": "1920 Commons",
+          "isOpen": true,
+          "venueID": 593,
+        },
+        {
+          "name": "Hill",
+          "isOpen": true,
+          "venueID": 636,
+        },
+      ],
+    }, {
+      "title": "Closed",
+      "links": [
+        {
+          "name": "Kings Court",
+          "isOpen": false,
+        },
+        {
+          "name": "New College House",
+          "isOpen": false,
+        },
+        {
+          "name": "Bridge",
+          "isOpen": false,
+        },
+      ],
+    }]
+  }
+
+  static propTypes = {
+    sections: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        links: PropTypes.arrayOf(
+          PropTypes.shape({
+            name: PropTypes.string,
+            isOpen: PropTypes.bool,
+            venueID: PropTypes.number
+          })
+        )
+      })
+    )
+  }
+
+  renderSections() {
+    return this.props.sections.map(section =>
+      <SidebarSection
+        {...section}
+        key={uuid()}
+      />
+    );
+  }
+
   render() {
-    const sections = [
-      {
-        "title": "Open now",
-        "links": [
-          {
-            "name": "1920 Commons",
-            "isOpen": true,
-            "venueID": 593,
-          },
-          {
-            "name": "Hill",
-            "isOpen": true,
-            "venueID": 636,
-          },
-        ],
-      },
-      {
-        "title": "Closed",
-        "links": [
-          {
-            "name": "Kings Court",
-            "isOpen": false,
-          },
-          {
-            "name": "New College House",
-            "isOpen": false,
-          },
-          {
-            "name": "Bridge",
-            "isOpen": false,
-          },
-        ],
-      }
-    ]
-
-    const content = sections.map((section, index) => {
-      return <SidebarSection title={section.title} links={section.links} key={index} />;
-    });
-
     return (
       <div className="sidebar" id="sidebar">
-        {content}
+        {this.renderSections()}
       </div>
     )
   }
