edcedc20b19511bca155cc468c51f5abb6c2d2f5
ccabo1 <ccabo@seas.upenn.edu>
2018-02-23 18:35:36 -0500

    Bug buster 🐛

--- src/shared/components/App.js
+++ src/shared/components/App.js
@@ -44,5 +44,3 @@ export default () => (
     <Footer />
   </div>
 );
-
-export default App;
--- src/shared/components/home/Notification.js
+++ src/shared/components/home/Notification.js
@@ -29,7 +29,7 @@ class Notification extends Component {
 // Prop validations
 Notification.propTypes = {
   show: PropTypes.func,
-  text: PropTypes.string,
+  text: PropTypes.array,
 };
 
 export default Notification;
