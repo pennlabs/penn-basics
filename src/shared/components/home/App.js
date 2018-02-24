3d49d1e5ebfc383576003c15b5981ed5803a9374
Tiffany <tifchang@wharton.upenn.edu>
2018-02-23 19:28:03 -0500

    removed rar, going to Nihar's GSR component

--- src/shared/components/home/App.js
+++ src/shared/components/home/App.js
@@ -12,7 +12,7 @@ class Home extends Component {
   constructor(props) {
     super (props);
       this.state = {
-        show: true,
+        show: false,
         notification: [],
         dining: false,
       }
--- src/shared/components/home/Reserve.js
+++ src/shared/components/home/Reserve.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import '../../styles/home.scss';
-import RARCards from './RARCards';
 
 class Reserve extends Component {
   render() {
@@ -10,11 +9,6 @@ class Reserve extends Component {
         <h1 className="title is-3">Reserve a room</h1>
         <h2 className="subtitle is-5">Book a room quickly across campus: at VP,
           Huntsman, Ed Commons, and more.</h2>
-        <RARCards />
-        <RARCards />
-        <RARCards />
-        <RARCards />
-        <RARCards />
       </div>
     )
   }
