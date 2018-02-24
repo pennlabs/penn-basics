cd43d7067f33221dcdf55ae366f3769881427b73
Tiffany <tifchang@wharton.upenn.edu>
2018-02-23 19:51:47 -0500

    fixed studyspaces linter issues

---
+++ src/shared/components/studyspaces/StudySpacesVenue.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class StudySpacesVenue extends Component {
+  render() {
+    return(
+      <div className="columns">
+        <div className="column">
+          First column
+        </div>
+        <div className="column">
+          Second column
+        </div>
+        <div className="column">
+          Third column
+        </div>
+        <div className="column">
+          Fourth column
+        </div>
+      </div>
+    );
+  }
+}
+export default StudySpacesVenue;
