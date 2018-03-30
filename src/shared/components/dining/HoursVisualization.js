15dfa890ab1484f6cbaab4cf7a42f4a42bc524bf
ccabo1 <ccabo@seas.upenn.edu>
2018-03-30 17:19:28 -0400

    updates to hours viz

--- src/shared/components/dining/HoursVisualization.js
+++ src/shared/components/dining/HoursVisualization.js
@@ -90,6 +90,28 @@ class DiningOverview extends Component {
     return formattedTime + " AM";
   }
 
+  updateHours(date, time) {
+    // Update the hours of the passed in date to the time passed in along with it
+    // NOTE time is of the form "HH:MM:SS"
+    const hours = time.substring(0, time.indexOf(":"));
+    const minutes = time.substring(time.indexOf(":" + 1), time.lastIndexOf(":"));
+    date.setHours(hours, minutes, 0, 0);
+  }
+
+  isRightNow(meal) {
+    if (!meal) return false;
+
+    // Return true if the meal is being served right now
+    const currentTime = Date.now();
+    const mealStart = new Date(meal.date);
+    this.updateHours(mealStart, meal.open);
+    const mealEnd = new Date(meal.date);
+    this.updateHours(mealEnd, meal.close);
+    const start = mealStart.getTime();
+    const end = mealEnd.getTime();
+    return (start < currentTime && end > currentTime);
+  }
+
   getDay(date) {
     // Return the day as a string of the passed in date
     // Note the passed in date is in Date.toString() format
@@ -116,9 +138,8 @@ class DiningOverview extends Component {
         <tbody>
           {
             this.props.venueHours.map(meal => {
-              console.log(meal);
               return (
-                <tr key={uuid()}>
+                <tr key={uuid()} className={this.isRightNow(meal) ? "is-selected" : ""}>
                   <td>{this.getDay(meal.date)}</td>
                   <td>{meal.type}</td>
                   <td>{this.cleanTime(meal.open)}</td>
