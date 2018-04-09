fe6d60e0843c282bd729ff9e0386a75b226140bc
Tiffany <tifchang@wharton.upenn.edu>
2018-04-09 16:36:18 -0400

    env.sh file

--- src/shared/components/home/Dining.js
+++ src/shared/components/home/Dining.js
@@ -16,20 +16,20 @@ class Dining extends Component {
 
   render() {
     return (
-        <div className="tile is-child box">
-          <Link to={`/dining`} className="link">
-            <h1 className="title is-4">Dining</h1>
-            <h2 className="subtitle is-6">Dining description placeholder.</h2>
-          </Link>
-          <div className="space-1"></div>
-          {
-            this.state.object == null
-              ? <img src="https://i.imgur.com/fFniYax.png" width="500px" />
-              : <div>
-                <DiningCards name={"Kings Court English House"} type={0} />
-                <DiningCards name={"Pret a Manger"} type={1} />
-              </div>
-          }
+      <div className="tile is-child box">
+        <Link to={`/dining`} className="link">
+          <h1 className="title is-4">Dining</h1>
+          <h2 className="subtitle is-6">Dining description placeholder.</h2>
+        </Link>
+        <div className="space-1"/>
+        {
+          this.state.object === null
+            ? <img src="https://i.imgur.com/fFniYax.png" width="500px" />
+            : <div>
+              <DiningCards name={"Kings Court English House"} type={0} />
+              <DiningCards name={"Pret a Manger"} type={1} />
+            </div>
+        }
       </div>
     );
   }
