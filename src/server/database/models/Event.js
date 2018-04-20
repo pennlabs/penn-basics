4e31ec6bf05a4d7491d6bbdff7914686e8a41399
Nihar Patil <nihar.patil98@gmail.com>
2018-04-20 17:18:40 -0400

    Create Event model

---
+++ src/server/database/models/Event.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema({
+  start: {
+    required: true,
+    type: Date
+  },
+  end: {
+    required: true,
+    type: Date
+  },
+  event: {
+    required: true,
+    type: String
+  },
+});
+
+module.exports = mongoose.model('Event', EventSchema);
