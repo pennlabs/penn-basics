4e4cf8b91f98217af237437c771a95a89cb2f10b
ccabo1 <ccabo@seas.upenn.edu>
2018-11-09 18:31:47 -0500

    Adding padding back to made with labs jawn

--- src/shared/components/studyspaces/App.js
+++ src/shared/components/studyspaces/App.js
@@ -50,12 +50,14 @@ class App extends Component {
             );
           })}
 
-          <Subtext paddingTop="0.5rem" marginBottom="0">
-            Made with &hearts; by&nbsp;
-            <a href="https://pennlabs.org" target="_BLANK" rel="noopener noreferrer">
-              Penn Labs.
-            </a>
-          </Subtext>
+          <Col padding="0 1rem">
+            <Subtext paddingTop="0.5rem" marginBottom="0">
+              Made with &hearts; by&nbsp;
+              <a href="https://pennlabs.org" target="_BLANK" rel="noopener noreferrer">
+                Penn Labs.
+              </a>
+            </Subtext>
+          </Col>
         </Col>
         <Col>
           <Map
