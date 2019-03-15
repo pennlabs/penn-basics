5036b5c91c5cea44bdb0a4e714791b0575b4f06c
niharpatil <nihar.patil98@gmail.com>
2019-03-15 17:56:25 -0400

    add different rendering for retail vs dining hall

--- src/shared/components/dining/App.js
+++ src/shared/components/dining/App.js
@@ -4,6 +4,10 @@ import Nav from './Nav';
 
 export default () => (
   <Nav>
-    <p>Todo</p>
+    <div className="columns is-vcentered is-centered" style={{ height: '100%' }}>
+      <div className="column is-7">
+        <h1 className="title is-3" style={{color: '#aaaaaa'}}>Select a dining hall to see information</h1>
+      </div>
+    </div>
   </Nav>
 );
--- src/shared/components/dining/Nav.js
+++ src/shared/components/dining/Nav.js
@@ -19,11 +19,42 @@ import { NAV_HEIGHT } from '../../styles/sizes'
 
 import venueData from '../../../server/database/venue_info.json'
 
+import { retailLocations } from './constants'
+
 const StyledLink = s(Link)`
   h2 {
     color: ${DARK_GRAY} !important;
   }
 `
+
+function diningCard(key, name, image) {
+  const img = `/img/venue_images/${image}`;
+
+  return (
+    <StyledLink to={`/dining/${key}`} key={key}>
+      <Card padding="0.5rem 1rem" hoverable>
+        <Row>
+          {image && (
+            <Col backgroundImage={img} width="30%" borderRadius="4px" />
+          )}
+          <Col
+            padding={image ? '0.5rem 0 0.5rem 1rem' : '0'}
+          >
+            <Subtitle marginBottom="0">
+              {name}
+            </Subtitle>
+
+            <Subtext marginBottom="0">
+              This is some subtext
+            </Subtext>
+          </Col>
+        </Row>
+      </Card>
+
+      <Line />
+    </StyledLink>
+  );
+}
 
 const Nav = ({ children }) => (
   <Row maxHeight={`calc(100vh - ${NAV_HEIGHT})`}>
@@ -36,34 +67,16 @@ const Nav = ({ children }) => (
     >
       {Object.keys(venueData).map((key) => {
         const { name, image } = venueData[key];
+        if (!retailLocations.includes(name)) {
+          return diningCard(key, name, image)
+        }
+      })}
 
-        const img = `/img/venue_images/${image}`;
-
-        return (
-          <StyledLink to={`/dining/${key}`} key={key}>
-            <Card padding="0.5rem 1rem" hoverable>
-              <Row>
-                {image && (
-                  <Col backgroundImage={img} width="30%" borderRadius="4px" />
-                )}
-                <Col
-                  padding={image ? '0.5rem 0 0.5rem 1rem' : '0'}
-                  onMouseEnter={this.handleMouseEnter}
-                >
-                  <Subtitle marginBottom="0">
-                    {name}
-                  </Subtitle>
-
-                  <Subtext marginBottom="0">
-                    This is some subtext
-                  </Subtext>
-                </Col>
-              </Row>
-            </Card>
-
-            <Line />
-          </StyledLink>
-        );
+      {Object.keys(venueData).map((key) => {
+        const { name, image } = venueData[key];
+        if (retailLocations.includes(name)) {
+          return diningCard(key, name, image)
+        }
       })}
 
       <PennLabsCredit />
