87a14ae85f5de6c917f657d8c85dfa2b59936afd
ccabo1 <ccabo@seas.upenn.edu>
2019-03-24 12:58:50 -0400

    cleaning up modal code

--- src/shared/components/shared/Modal.js
+++ src/shared/components/shared/Modal.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import styled, { keyframes } from 'styled-components';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styled, { keyframes } from 'styled-components'
 
 import {
   WHITE,
   LIGHT_GRAY,
   SNOW_ALPHA,
   SHADOW,
-} from '../../styles/colors';
+} from '../../styles/colors'
 
-const Z_INDEX = 1200;
-const ANIMATION_DURATION = '0.4s';
+const Z_INDEX = 1200
+const ANIMATION_DURATION = '0.4s'
+const LG_WIDTH = 50
+const SM_WIDTH = 80
+
 
 // TODO mobile responsiveness
 // TODO use refactored modal code
+
 
 const fadeIn = keyframes`
   0% {
@@ -25,7 +29,8 @@ const fadeIn = keyframes`
     opacity: 1;
     max-height: 100vh;
   }
-`;
+`
+
 
 const fadeOut = keyframes`
   0% {
@@ -37,7 +42,8 @@ const fadeOut = keyframes`
     opacity: 0;
     max-height: 100vh;
   }
-`;
+`
+
 
 const slideIn = keyframes`
   0% {
@@ -45,19 +51,21 @@ const slideIn = keyframes`
   }
 
   100% {
-    margin-left: 50%;
+    margin-left: ${LG_WIDTH}%;
   }
-`;
+`
+
 
 const slideOut = keyframes`
   0% {
-    margin-left: 50%;
+    margin-left: ${LG_WIDTH}%;
   }
 
   100% {
     margin-right: 100%;
   }
-`;
+`
+
 
 const slideInMobile = keyframes`
   0% {
@@ -65,19 +73,21 @@ const slideInMobile = keyframes`
   }
 
   100% {
-    margin-left: 25%;
+    margin-left: ${SM_WIDTH}%;
   }
-`;
+`
+
 
 const slideOutMobile = keyframes`
   0% {
-    margin-left: 25%;
+    margin-left: ${SM_WIDTH}%;
   }
 
   100% {
     margin-right: 100%;
   }
-`;
+`
+
 
 const ModalWrapper = styled.div`
   position: fixed;
@@ -104,7 +114,8 @@ const ModalWrapper = styled.div`
   animation-duration: ${ANIMATION_DURATION};
   max-height: ${({ show }) => (show ? '100vh' : '0vh')};
   opacity: ${({ show }) => (show ? '1' : '0')};
-`;
+`
+
 
 const ModalContent = styled.div`
   min-height: 100%;
@@ -121,7 +132,8 @@ const ModalContent = styled.div`
     margin-left: ${({ show }) => (show ? '25%' : '100%')};
     animation-name: ${({ show }) => (show ? slideInMobile : slideOutMobile)};
   }
-`;
+`
+
 
 const ModalClose = styled.div`
   position: fixed;
@@ -154,55 +166,56 @@ const Times = styled.span`
 
 // Do not propagate events on the modal content to the modal background
 // This would otherwise cause the modal to close on any click
-const noop = event => event.stopPropagation();
+const noop = event => event.stopPropagation()
+
 
 export class Modal extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = {
-      isNewlyMounted: true,
-    };
+    this.state = { isNewlyMounted: true }
 
-    this.focusRef = React.createRef();
+    this.focusRef = React.createRef()
 
-    this.makeNotNewlyMounted = this.makeNotNewlyMounted.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.makeNotNewlyMounted = this.makeNotNewlyMounted.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
   }
+
 
   // Avoid animations showing on load
   componentDidUpdate(prevProps) {
-    const { show } = this.props;
-    const { isNewlyMounted } = this.state;
+    const { show } = this.props
+    const { isNewlyMounted } = this.state
 
     // If this is the first time the modal is mounting, engage the animation
     if (isNewlyMounted && prevProps.show !== show) {
-      this.makeNotNewlyMounted();
+      this.makeNotNewlyMounted()
     }
 
     // If we are showing the modal, focus on it
     if (show && !prevProps.show) {
-      this.focusRef.current.focus();
+      this.focusRef.current.focus()
     }
   }
 
+
   makeNotNewlyMounted() {
-    this.setState({
-      isNewlyMounted: false,
-    });
+    this.setState({ isNewlyMounted: false })
   }
+
 
   // Close the modal when the user presses the escape key
   handleKeyPress(event) {
-    const ESCAPE_KEY_CODE = 27;
-    const { show } = this.props;
+    const ESCAPE_KEY_CODE = 27
+    const { show } = this.props
 
     if (event.keyCode === ESCAPE_KEY_CODE && show) {
-      const { toggle } = this.props;
+      const { toggle } = this.props
 
-      toggle();
+      toggle()
     }
   }
+
 
   render() {
     const { show, toggle, children } = this.props;
@@ -230,15 +243,17 @@ export class Modal extends Component {
   }
 }
 
+
 Modal.propTypes = {
   show: PropTypes.bool.isRequired,
   toggle: PropTypes.func.isRequired,
   children: PropTypes.any.isRequired, // eslint-disable-line
-};
+}
+
 
 export const ModalContainer = styled.div`
   padding: 0 8.33vw;
   background: ${({ background }) => background || WHITE};
   padding-top: ${({ paddingTop }) => paddingTop || 0};
   padding-bottom: ${({ paddingBottom }) => paddingBottom || 0};
-`;
+`
