01bfa2af1f4f808f4d7cbdc8df9b2610a45abd4b
peterbaile <peterchenwflms@gmail.com>
2019-03-31 12:49:30 -0400

    merge refactoring

---
+++ src/shared/components/studyspaces/StudySpacesVenue.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const StudySpacesVenue = () => (
+  <div className="columns">
+    <div className="column">
+      First column
+    </div>
+    <div className="column">
+      Second column
+    </div>
+    <div className="column">
+      Third column
+    </div>
+    <div className="column">
+      Fourth column
+    </div>
+  </div>
+);
+
+export default StudySpacesVenue;
