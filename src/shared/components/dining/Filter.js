f191e65cfbf30a337e2dc6e166caca15d7dcab31
peterbaile <peterchenwflms@gmail.com>
2019-04-07 12:47:10 -0400

    update laundry action

---
+++ src/shared/components/dining/Filter.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import s from 'styled-components'
+import PropTypes from 'prop-types'
+
+class Filter extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        
+    }
+}
+
+export default Filter
