af2335417cdeb941e5b2fc32453dd3815862aede
ccabo1 <ccabo@seas.upenn.edu>
2019-09-07 13:09:21 -0400

    fixed some nav stuff

--- src/shared/components/App.js
+++ src/shared/components/App.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import s from 'styled-components'
 import { MobileView, BrowserView } from 'react-device-detect'
 
-
 import { BLUE, DARK_BLUE } from '../styles/colors'
 
-
-// import Sidebar from './shared/sidebar/Sidebar'; /* TODO */
 import Nav from './shared/Nav/index'
-
-
 import Home from './home/App'
 import NotFound from './shared/NotFound'
 import Dining from './dining/DiningVenue'
@@ -18,7 +13,6 @@ import Laundry from './laundry/App'
 import StudySpaces from './studyspaces/App'
 import Reservations from './reservations/App'
 import Mobile from './mobile/App'
-
 
 const App = s.div`
   a {
@@ -48,7 +42,7 @@ export default () => (
       <div id="wrapper">
         <div id="app">
           <Switch>
-            <Route exact path="/" component={Home}/>
+            <Route exact path="/" component={Home} />
             <Route exact path="/dining" component={Dining} />
             <Route exact path="/dining/:id" component={Dining} />
             <Route exact path="/laundry" component={Laundry} />
@@ -61,4 +55,4 @@ export default () => (
       </div>
     </BrowserView>
   </App>
-);
+)
--- src/shared/components/shared/WrappedLink.js
+++ src/shared/components/shared/WrappedLink.js
@@ -2,44 +2,56 @@
 // react-router.  The reason we use this component is we want to update our redux
 // store with our current link each time we click a `Link`
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import s from 'styled-components'
 
-import { updateLink } from '../../actions/action_types';
+import { BLUE, DARK_BLUE } from '../../styles/colors'
+import { updateLink } from '../../actions/action_types'
 
-class WrappedLink extends Component { // eslint-disable-line
+const StyledLink = s(Link)`
+  color: ${BLUE} !important;
+
+  &:active,
+  &:focus,
+  &:hover {
+    color: ${DARK_BLUE} !important;
+  }
+`
+
+class WrappedLink extends Component {
+  // eslint-disable-line
   render() {
-    const {
-      updateReduxWithPath,
-      to,
-      ...props
-    } = this.props;
+    const { updateReduxWithPath, to, ...props } = this.props
 
     return (
-      <Link
+      <StyledLink
         tabIndex={0}
         onClick={() => updateReduxWithPath(to)}
         to={to}
         {...props}
       />
-    );
+    )
   }
 }
 
 const mapDispatchToProps = dispatch => ({
-  updateReduxWithPath: (to) => {
+  updateReduxWithPath: to => {
     dispatch({
       type: updateLink,
       payload: to,
-    });
+    })
   },
-});
+})
 
 WrappedLink.propTypes = {
   to: PropTypes.any, // eslint-disable-line
   updateReduxWithPath: PropTypes.func.isRequired,
-};
+}
 
-export default connect(() => ({}), mapDispatchToProps)(WrappedLink);
+export default connect(
+  () => ({}),
+  mapDispatchToProps
+)(WrappedLink)
