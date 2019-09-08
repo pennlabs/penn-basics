1bf4531a0baa30d65b04b93051de41aefb84d247
ccabo1 <ccabo@seas.upenn.edu>
2019-09-08 13:38:21 -0400

    fix map controls

--- src/shared/components/shared/Map.js
+++ src/shared/components/shared/Map.js
@@ -1,133 +1,136 @@
 /* global google, document */
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
-const SHOW_MARKER_KEY = -1; // Marker keys which we shouldn't delete
-const RED = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
-const BLUE = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+const SHOW_MARKER_KEY = -1 // Marker keys which we shouldn't delete
+const RED = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+const BLUE = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
 
 const MapWrapper = styled.div`
   width: 100%;
   flex: 1;
   height: ${({ height }) => height || '100%'};
-`;
+`
 
 export class Map extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       map: null,
       markers: {},
-    };
+    }
 
-    this.waitForGoogle = this.waitForGoogle.bind(this);
-    this.createMarker = this.createMarker.bind(this);
-    this.updateMarkers = this.updateMarkers.bind(this);
+    this.waitForGoogle = this.waitForGoogle.bind(this)
+    this.createMarker = this.createMarker.bind(this)
+    this.updateMarkers = this.updateMarkers.bind(this)
   }
 
   componentDidMount() {
-    this.waitForGoogle();
+    this.waitForGoogle()
   }
 
   componentDidUpdate(prevProps) {
     // Check if the active marker changes
-    const { activeMarker } = this.props;
-    const oldActiveMarker = prevProps.activeMarker;
+    const { activeMarker } = this.props
+    const oldActiveMarker = prevProps.activeMarker
 
     if (activeMarker !== oldActiveMarker) {
-      this.updateMarker(oldActiveMarker, { icon: RED });
-      this.updateMarker(activeMarker, { icon: BLUE });
+      this.updateMarker(oldActiveMarker, { icon: RED })
+      this.updateMarker(activeMarker, { icon: BLUE })
     }
 
     // Check if the data changed and update markers
-    const { markers } = this.props;
+    const { markers } = this.props
 
     if (markers !== prevProps.markers) {
-      this.updateMarkers();
+      this.updateMarkers()
     }
   }
 
   updateMarkers() {
-    return new Promise((resolve) => {
-      const { markers: dataMarkers = {} } = this.props;
-      const { markers: mapMarkers = {} } = this.state;
+    return new Promise(resolve => {
+      const { markers: dataMarkers = {} } = this.props
+      const { markers: mapMarkers = {} } = this.state
 
-      const dataKeys = Object.keys(dataMarkers);
-      const mapKeys = Object.keys(mapMarkers);
+      const dataKeys = Object.keys(dataMarkers)
+      const mapKeys = Object.keys(mapMarkers)
 
-      mapKeys.forEach((key) => {
+      mapKeys.forEach(key => {
         if (!dataKeys[key] && key !== SHOW_MARKER_KEY) {
           // Delete the marker on the map and remove it from the object
-          const marker = mapMarkers[key];
-          marker.setMap(null);
-          delete mapMarkers[key];
+          const marker = mapMarkers[key]
+          marker.setMap(null)
+          delete mapMarkers[key]
         }
-      });
+      })
 
-      dataKeys.forEach((key) => {
+      dataKeys.forEach(key => {
         if (!mapKeys[key]) {
           // Create a marker new marker and add it to the state
-          const marker = this.createMarker(key, dataMarkers[key]);
-          mapMarkers[key] = marker;
+          const marker = this.createMarker(key, dataMarkers[key])
+          mapMarkers[key] = marker
         }
-      });
+      })
 
       // Update the state
-      this.setState({
-        markers: mapMarkers,
-      }, () => {
-        resolve(true);
-      });
-    });
+      this.setState(
+        {
+          markers: mapMarkers,
+        },
+        () => {
+          resolve(true)
+        }
+      )
+    })
   }
 
   updateMarker(key, { icon = RED }) {
-    const { markers } = this.state;
-    const marker = markers[key];
+    const { markers } = this.state
+    const marker = markers[key]
 
-    if (!marker) return;
+    if (!marker) return
 
-    marker.setIcon(icon); // TODO this might not work
+    marker.setIcon(icon) // TODO this might not work
   }
 
   createMarker(key, { location, icon = RED }) {
-    const { handleClickMarker } = this.props;
+    const { handleClickMarker } = this.props
 
     if (!location) {
-      console.log('Location is undefined'); // eslint-disable-line no-console
+      console.log('Location is undefined') // eslint-disable-line no-console
 
-      return null;
+      return null
     }
 
-    const { lat, lng } = location;
+    const { lat, lng } = location
 
     if (typeof lat === 'undefined' || typeof lng === 'undefined') {
-      console.log('Lat or lng is undefined'); // eslint-disable-line no-console
+      console.log('Lat or lng is undefined') // eslint-disable-line no-console
 
-      return null;
+      return null
     }
 
-    const { map } = this.state;
+    const { map } = this.state
 
     if (!map) {
-      console.log('Map is undefined'); // eslint-disable-line no-console
+      console.log('Map is undefined') // eslint-disable-line no-console
 
-      return null;
+      return null
     }
 
     const marker = new google.maps.Marker({
       position: location,
       icon,
       map,
-    });
+    })
 
     if (handleClickMarker) {
       marker.addListener('click', () => handleClickMarker(key))
     }
 
-    return marker;
+    return marker
   }
 
   initMap() {
@@ -136,52 +139,55 @@ export class Map extends Component {
       mapId = 'map',
       gestureHandling = '',
       showMarker = false,
-    } = this.props;
+    } = this.props
 
     const map = new google.maps.Map(document.getElementById(mapId), {
       center: location,
       zoom: 15,
       gestureHandling,
+      streetViewControl: false, // Disable street view button + orange dude
+      fullscreenControl: false, // Disable full screen button
+      zoomControl: false, // Disable zoom buttons
       mapTypeControlOptions: {
         mapTypeIds: [],
       },
-    });
+    })
 
-    this.setState({
-      map,
-    }, () => {
-      this.updateMarkers()
-        .then(() => {
+    this.setState(
+      {
+        map,
+      },
+      () => {
+        this.updateMarkers().then(() => {
           if (showMarker) {
             const marker = this.createMarker(SHOW_MARKER_KEY, { location })
 
             const { markers } = this.state
             const newMarkers = Object.assign({}, markers, {
               SHOW_MARKER_KEY: marker,
-            });
+            })
             markers[SHOW_MARKER_KEY] = marker
 
             this.setState({ markers: newMarkers })
           }
-        });
-    });
+        })
+      }
+    )
   }
 
   waitForGoogle() {
     if (typeof google !== 'undefined') {
-      this.initMap();
+      this.initMap()
     } else {
       // Check again if google is defined
-      setTimeout(this.waitForGoogle, 125);
+      setTimeout(this.waitForGoogle, 125)
     }
   }
 
   render() {
-    const { height, mapId } = this.props;
+    const { height, mapId } = this.props
 
-    return (
-      <MapWrapper height={height} id={mapId} />
-    );
+    return <MapWrapper height={height} id={mapId} />
   }
 }
 
@@ -196,7 +202,7 @@ Map.defaultProps = {
   showMarker: false,
   activeMarker: null,
   handleClickMarker: null,
-};
+}
 
 Map.propTypes = {
   location: PropTypes.shape({
@@ -210,4 +216,4 @@ Map.propTypes = {
   markers: PropTypes.object, // eslint-disable-line
   showMarker: PropTypes.bool,
   activeMarker: PropTypes.string,
-};
+}
--- src/shared/components/shared/PennLabsCredit.js
+++ src/shared/components/shared/PennLabsCredit.js
@@ -3,12 +3,17 @@ import React from 'react'
 import { Col } from './Flex'
 import { Subtext } from './Typography'
 
+const AIRTABLE_LINK = 'https://airtable.com/shrE9ZdgiSY0DfDxV'
+
 export default () => (
   <Col padding="0 1rem">
     <Subtext paddingTop="0.5rem" marginBottom="0">
       Made with &hearts; by&nbsp;
       <a href="https://pennlabs.org" target="_BLANK" rel="noopener noreferrer">
         Penn Labs.
+      </a>{' '}
+      <a href={AIRTABLE_LINK} taget="_BLANK" rel="noopener noreferrer">
+        Feedback.
       </a>
     </Subtext>
   </Col>
