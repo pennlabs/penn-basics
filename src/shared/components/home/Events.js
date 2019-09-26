68d7395725c6d308738798414a56f87bc316a6f7
peterbaile <peterchenwflms@gmail.com>
2019-09-26 14:54:04 -0400

    refactor events and quotes

--- src/shared/components/home/Events.js
+++ src/shared/components/home/Events.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import uuid from 'uuid'
 import moment from 'moment'
@@ -6,74 +6,68 @@ import { BorderedCard } from '../shared'
 
 const GET_EVENTS_ROUTE = 'https://api.pennlabs.org/calendar/'
 
-class Events extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      calendarArray: null,
-    }
-  }
-
-  componentDidMount() {
-    axios.get(GET_EVENTS_ROUTE).then(res => {
-      const calendarArray = res.data.calendar
-      this.setState({ calendarArray })
-    })
-  }
-
-  renderCalendarCards() {
-    const { calendarArray } = this.state
-    return calendarArray.map(event => {
-      const { start, name } = event
-      const startDate = moment(start).format('dddd[,] MMMM Do')
-      return (
-        <article className="media" key={uuid()}>
-          <div className="media-left">
-            <figure className="image is-64x64">
-              <img
-                src="https://pbs.twimg.com/profile_images/875383884862570496/TN7FoDDx.jpg"
-                alt="First"
-              />
-            </figure>
-          </div>
-          <div className="spacer-20" />
-          <div className="media-content">
-            <div className="content">
-              <p className="is-size-6">
-                <strong>{name}</strong>
-                <br />
-                <small>
-                  Starts from
-                  {startDate}
-                </small>
-              </p>
-            </div>
-          </div>
-        </article>
-      )
-    })
-  }
-
-  renderSubtext() {
-    const { calendarArray } = this.state
-    if (calendarArray.length === 0)
+const Subtext = ({ length }) => {
+  switch (length) {
+    case 0:
       return 'No Events happening in next two weeks'
-    return `${calendarArray.length} Events happening in next two weeks`
+    case 1:
+      return '1 Event happening in next two weeks'
+    default:
+      return `${length} Events happening in next two weeks`
   }
+}
 
-  render() {
-    const { calendarArray } = this.state
+const Events = () => {
+  const [calendarArray, setCalendar] = useState(null)
 
-    return (
-      <BorderedCard>
-        <h1 className="title is-4">University Calendar</h1>
-        {calendarArray && (
-          <h2 className="subtitle is-6"> {this.renderSubtext()} </h2>
-        )}
-        {calendarArray && this.renderCalendarCards()}
-      </BorderedCard>
-    )
-  }
+  useEffect(() => {
+    const cancelToken = axios.CancelToken
+    const source = cancelToken.source()
+    axios
+      .get(GET_EVENTS_ROUTE, { cancelToken: source.token })
+      .then(res => setCalendar(res.data.calendar))
+  }, [])
+
+  if (!calendarArray) return null
+
+  return (
+    <BorderedCard>
+      <h1 className="title is-4">University Calendar</h1>
+
+      <h2 className="subtitle is-6">
+        <Subtext length={calendarArray.length} />
+      </h2>
+      {calendarArray.map(event => {
+        const { start, name } = event
+        const startDate = moment(start).format('dddd[,] MMMM Do')
+        return (
+          <article className="media" key={uuid()}>
+            <div className="media-left">
+              <figure className="image is-64x64">
+                <img
+                  src="https://pbs.twimg.com/profile_images/875383884862570496/TN7FoDDx.jpg"
+                  alt="First"
+                />
+              </figure>
+            </div>
+            <div className="spacer-20" />
+            <div className="media-content">
+              <div className="content">
+                <p className="is-size-6">
+                  <strong>{name}</strong>
+                  <br />
+                  <small>
+                    Starts from
+                    {startDate}
+                  </small>
+                </p>
+              </div>
+            </div>
+          </article>
+        )
+      })}
+    </BorderedCard>
+  )
 }
 
 export default Events
--- src/shared/components/home/Quotes.js
+++ src/shared/components/home/Quotes.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import s from 'styled-components'
 import quotes from '../../../server/resources/home/homepage_quotes.json'
 
@@ -17,39 +17,33 @@ const greetings = [
 
 const emojis = ['😀', '😛', '😗', '🤠']
 
-class Quotes extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      greeting: '',
-      quote: '',
-      author: '',
-    }
-  }
+const Quotes = () => {
+  const [greetingState, setGreeting] = useState('')
+  const [quoteState, setQuote] = useState('')
+  const [authorState, setAuthor] = useState('')
 
-  componentDidMount() {
+  useEffect(() => {
     const quote = quotes[Math.floor(Math.random() * quotes.length)]
     const greeting = greetings[Math.floor(Math.random() * greetings.length)]
     const emoji = emojis[Math.floor(Math.random() * emojis.length)]
-    this.setState({ ...quote, greeting: `${greeting} ${emoji}` })
-  }
+    setGreeting(`${greeting} ${emoji}`)
+    setQuote(quote.quote)
+    setAuthor(quote.author)
+  }, [])
 
-  render() {
-    const { quote, author, greeting } = this.state
-    return (
-      <>
-        <h1 className="title is-4">{greeting}</h1>
-        <div className="content is-medium" style={{ marginTop: '0.5rem' }}>
-          <p className="has-text-centered">{`"${quote}"`}</p>
-          <Author className="has-text-right">
-            &#8212;
-            {` `}
-            {author}
-          </Author>
-        </div>
-      </>
-    )
-  }
+  return (
+    <>
+      <h1 className="title is-4">{greetingState}</h1>
+      <div className="content is-medium" style={{ marginTop: '0.5rem' }}>
+        <p className="has-text-centered">{`"${quoteState}"`}</p>
+        <Author className="has-text-right">
+          &#8212;
+          {` `}
+          {authorState}
+        </Author>
+      </div>
+    </>
+  )
 }
 
 export default Quotes
