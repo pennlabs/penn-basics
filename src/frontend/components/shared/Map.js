a1665a47e61064e691bfc4804de06ba69d865075
peterbaile <peterchenwflms@gmail.com>
2019-11-03 17:13:57 -0500

    change map marker

--- src/frontend/components/shared/Map.js
+++ src/frontend/components/shared/Map.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
 const SHOW_MARKER_KEY = -1 // Marker keys which we shouldn't delete
+// const RED = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
 const RED = 'https://raw.githubusercontent.com/pennlabs/pennbasics/foodtrucks/public/img/food-pin.png'
 const BLUE = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
 
