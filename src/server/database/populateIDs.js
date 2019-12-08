48969385c9389e2f5fdb4d6353ffb1c9dbd73335
Phillip Hilliard <philliard3>
2019-12-08 12:57:31 -0500

    First draft of populateIDs script

---
+++ src/server/database/populateIDs.js
@@ -0,0 +1,21 @@
+const filename =
+  'C:\\Users\\pdhil\\Documents\\Code\\PennLabs\\pennbasics\\src\\server\\resources\\foodtrucks\\foodtrucks.json'
+const idField = 'foodtruckID'
+const uuid = require('uuid/v1')
+const fs = require('fs')
+
+const objs = JSON.parse(fs.readFileSync(filename).toString())
+console.log(objs)
+if (Array.isArray(objs)) {
+  const newObjs = objs.map(o => {
+    const newObj = { ...o }
+    if (newObj[idField] === undefined || newObj[idField] === null) {
+      newObj[idField] = uuid()
+    }
+    console.log(newObj[idField])
+    return newObj
+  })
+  console.log(newObjs[0])
+  process.exit(0)
+  fs.writeFileSync(filename, JSON.stringify(newObjs))
+}
