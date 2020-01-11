55b224b42dda183809d43860aa8bb24a9e200f8d
peterbaile <peterchenwflms@gmail.com>
2020-01-11 07:39:51 -0500

    move google maps script tag to <head>

--- pages/studyspaces.js
+++ pages/studyspaces.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 import App from '../src/frontend/components/studyspaces/App'
 import Header from '../src/frontend/components/header/index'
 
-const Laundry = () => {
+const Studyspaces = () => {
   const router = useRouter()
   const { id } = router.query
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY
 
   return (
     <Header>
+      <Head>
+        <script
+          src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`}
+        />
+      </Head>
       <App id={id} />
     </Header>
   )
 }
 
-export default Laundry
+export default Studyspaces
