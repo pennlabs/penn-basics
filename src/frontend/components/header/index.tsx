0ead78c952fc141843285b5b734a95c20a86cb9b
peterbaile <peterchenwflms@gmail.com>
2020-02-16 14:35:27 -0500

    fix google script API using typeof google

--- src/frontend/components/header/index.tsx
+++ src/frontend/components/header/index.tsx
@@ -5,7 +5,6 @@ import { createGlobalStyle } from 'styled-components'
 import Nav from '../shared/Nav'
 import Feedback from '../shared/Feedback'
 import { BLUE, DARK_BLUE, SNOW } from '../../styles/colors'
-import { GOOGLE_MAPS_API_ROUTE } from '../../../frontend/constants/routes'
 
 const GlobalStyle = createGlobalStyle`
   html, body {
@@ -29,10 +28,9 @@ const GlobalStyle = createGlobalStyle`
 
 interface IHeader {
   children: React.ReactNode | React.ReactNodeArray
-  GOOGLE_MAPS_API_KEY?: string
 }
 
-const Header = ({ children, GOOGLE_MAPS_API_KEY }: IHeader): React.ReactElement => (
+const Header = ({ children }: IHeader): React.ReactElement => (
   <>
     <Head>
       <meta charSet="utf-8" />
@@ -110,7 +108,6 @@ const Header = ({ children, GOOGLE_MAPS_API_KEY }: IHeader): React.ReactElement 
 
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="msapplication-TileColor" content="#5bbad5" />
-      {GOOGLE_MAPS_API_KEY && <script src={GOOGLE_MAPS_API_ROUTE(`${GOOGLE_MAPS_API_KEY}`)} />}
     </Head>
 
     <GlobalStyle />
