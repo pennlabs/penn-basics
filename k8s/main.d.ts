1ffb6207ddb54d6c522b255978dd41b1cacb7a78
Joy Liu <joyliu.q@gmail.com>
2022-03-25 17:44:11 -0400

    :fire: Untrack main.js and main.d.ts

--- k8s/main.d.ts
+++
@@ -1,5 +0,0 @@
-import { Construct } from 'constructs';
-import { PennLabsChart } from '@pennlabs/kittyhawk';
-export declare class MyChart extends PennLabsChart {
-    constructor(scope: Construct, id: string);
-}
