a95c8a48a2af731ba31613afe7a5f432425e2a2a
Joy Liu <joyliu.q@gmail.com>
2022-03-25 17:29:10 -0400

    :fire: Remove unnec files

---
+++ k8s/main.d.ts
@@ -0,0 +1,5 @@
+import { Construct } from 'constructs';
+import { PennLabsChart } from '@pennlabs/kittyhawk';
+export declare class MyChart extends PennLabsChart {
+    constructor(scope: Construct, id: string);
+}
