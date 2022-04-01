752159f1b5056ed068445ad5023c50a13247ddb6
Joy Liu <34288846+joyliu-q@users.noreply.github.com>
2022-04-01 16:55:19 -0400

    Use Kittyhawk for Deployment Config (#88)
    * :tada: Initial set-up
    
    * :art: Remove env
    
    * :art: Update dockerfile
    
    * :fire: Remove unnec files
    
    * :fire: Untrack main.js and main.d.ts
    
    * :fire: Remove Jest & test, update cdk8s.yaml
    
    * :arrow_up: Upgrade kittyhawk to v1.1.0
    
    * :art: Remove jest config from gitignore
    
    * Update
    
    * Update
    
    * Update
    
    Co-authored-by: Armaan Tobaccowalla <armaan@tobaccowalla.com>

---
+++ k8s/main.ts
@@ -0,0 +1,25 @@
+import { Construct } from 'constructs';
+import { PennLabsChart, ReactApplication } from '@pennlabs/kittyhawk';
+import { App } from 'cdk8s';
+
+export class MyChart extends PennLabsChart {
+  constructor(scope: Construct) {
+    super(scope);
+
+    const image = 'pennlabs/penn-basics';
+    const secret = 'penn-basics';
+    const domain = 'pennbasics.com';
+
+    new ReactApplication(this, 'react', {
+      deployment: {
+        image,
+        secret,
+      },
+      domain: { host: domain, paths: ['/'] },
+    });
+  }
+}
+
+const app = new App();
+new MyChart(app);
+app.synth();
